e4e9d8ae6320c51c931e9def897fc5a0289b9b3a
vasantsaladi <60614334+vasantsaladi@users.noreply.github.com>
2022-10-04 13:08:07 -0400

    Initial Commit

---
+++ Power Lifting App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Power Lifting App
+//
+//  Created by Vasant Saladi on 10/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Power Lifting AppUITests/Power_Lifting_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Power_Lifting_AppUITestsLaunchTests.swift
+//  Power Lifting AppUITests
+//
+//  Created by Vasant Saladi on 10/4/22.
+//
+
+import XCTest
+
+class Power_Lifting_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
